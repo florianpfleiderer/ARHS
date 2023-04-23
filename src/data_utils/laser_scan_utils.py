0e2a718b9daf360a6850b5017292d78a8f7bfbbc
Heinrich Fuhrmann <heinrich.fuhrmann@gmail.com>
2023-04-23 12:20:33 +0200

    Visualization of kinect and laser scan object detection done. Laser scan is quite inaccurate still, check if visualization is faulty.

--- src/data_utils/laser_scan_utils.py
+++ src/data_utils/laser_scan_utils.py
@@ -2,9 +2,14 @@
 
 from math import *
 from globals.globals import *
+from visualization.screen_components import *
 
 def laser_theta(i, laser_scan):
     return 180 * (1 + i * laser_scan.angle_increment / pi)
+
+def laser_alpha(laser_scan, theta, offset, height):
+    d = laser_scan.ranges[laser_index(theta, laser_scan)]
+    return offset - atand(height / d)
 
 def laser_index(theta, laser_scan):
     return int((theta / 180 - 1) * pi / laser_scan.angle_increment)
@@ -26,3 +31,13 @@ def range_denoise(ranges, kernel):
         new_ranges.append(mean)
 
     return new_ranges
+
+def show_laser(laser_scan, image):
+    laser_points = []
+    for i, r in enumerate(laser_scan.ranges):
+        theta = laser_theta(i, laser_scan)
+        alpha = laser_alpha(laser_scan, theta, KINECT_ANGLE, KINECT_HEIGHT)
+        laser_points.append(ScreenObject(theta, theta, alpha, alpha))
+
+    for laser_point in laser_points:
+        laser_point.draw(image)
