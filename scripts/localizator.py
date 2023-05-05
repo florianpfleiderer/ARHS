cd0d2c7a8620d24764d9012fd713f2fdb9c14ac5
Florian Pfleiderer <florian@pfleiderer.at>
2023-05-05 16:04:19 +0200

    added localisator node, field component messages dont work yet

---
+++ scripts/localizator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+
+import cv2
+from math import *
+import sys
+import time
+
+from player.msg import *
+
+class Localizator:
+
+    # need to detect 3 poles (check field components msg) to calculate the field dimensions
+    pass 
+
